aee59a6417df13e617c62821f12d14d8323bbb70
ahmedulger <ahmedulger@gmail.com>
2021-09-05 10:33:30 +0300

    Spring skeleton added

---
+++ src/main/java/com/ulger/kubernetesorchestrator/KubernetesOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.ulger.kubernetesorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesOrchestratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ulger/kubernetesorchestrator/KubernetesOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulger.kubernetesorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
